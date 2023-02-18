2751b3926ef6e634ef7c6903f29a55fe26f42e1b
Laura Ostering <laurabeepboop@Lauras-MacBook-Pro.local>
2023-02-17 21:20:02 -0700

    styling and API call

--- js/script.js
+++ js/script.js
@@ -2,9 +2,9 @@
 
 /* DROPDOWN BUTTON - https://www.w3schools.com/howto/howto_js_dropdown.asp
 When the user clicks on the button, toggle between hiding and showing the dropdown content */
-function myFunction() {
+function dropDown() {
     document.getElementById("myDropdown").classList.toggle("show");
-  }
+};
   
 // Close the dropdown menu if the user clicks outside of it 
 window.onclick = function(event) {
@@ -19,6 +19,15 @@ window.onclick = function(event) {
     };
 };
 
+/* functions for choosing difficulty */
+const easy = document.getElementById("easy");
+const medium = document.getElementById("medium");
+const hard = document.getAnimations("hard");
+
+
+
+// selectCategory();
+
 async function sendApiRequest() {
     let question = await fetch(`https://opentdb.com/api.php?amount=1&category=9&difficulty=easy&type=multiple`);
     console.log(question);
@@ -27,7 +36,30 @@ async function sendApiRequest() {
     useApiData(data);
 };
 
-// function useApiData(data) {
-//     const text = document.querySelector("text.geography");
-//     text.innerHTML = `${data.results[0].question}`;
-// }
+function useApiData(data) {
+    const wholeCard = document.querySelector("section.card");
+    wholeCard.innerHTML = `${data.results[0].question}`;
+    console.log(wholeCard)
+}
+
+const chooseCat = document.getElementsByClassName("category");
+console.log(chooseCat);
+
+/* on click, send API request */
+// Array.from(chooseCat).forEach((category) => {
+//   category.addEventListener('click', evt => {
+//     sendApiRequest();
+//   })
+// });
+
+const card = document.getElementById("card");
+
+Array.from(chooseCat).forEach((category) => {
+  category.addEventListener('click', evt => {
+    flipCard;
+    sendApiRequest();
+  })});
+
+function flipCard() {
+  card.classList.toggle("flipCard")
+};
