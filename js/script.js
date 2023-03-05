dbc452e0f01fab9c4a5f7677bcf1bbe249d3ecbf
Laura Ostering <laurabeepboop@Lauras-MacBook-Pro.local>
2023-03-05 15:01:32 -0700

    it works!

--- js/script.js
+++ js/script.js
@@ -1,5 +1,3 @@
-
-
 /* DROPDOWN BUTTON - https://www.w3schools.com/howto/howto_js_dropdown.asp
 When the user clicks on the button, toggle between hiding and showing the dropdown content */
 function dropDown() {
@@ -19,26 +17,73 @@ window.onclick = function(event) {
     };
 };
 
-
-/* functions for choosing difficulty */
+// Choose Difficulty Level
 const easy = document.getElementById("easy");
 const medium = document.getElementById("medium");
-const hard = document.getAnimations("hard");
+const hard = document.getElementById("hard");
+
+let difficulty = "easy"; // default difficulty
+
+easy.addEventListener('click', (evt) => {
+  difficulty = "easy";
+});
+
+medium.addEventListener('click', (evt) => {
+  difficulty = "medium";
+});
+
+hard.addEventListener('click', (evt) => {
+  difficulty = "hard";
+});
 
 
-// Send API Request, log data in console, Call Use API Data - https://www.youtube.com/watch?v=SgJ_femmsfg
+// Choose category and get question 
+function catGeo () {
+  choiceCat = 22; 
+  sendApiRequest();
+};
+
+function catEnt () {
+  choiceCat = 11; 
+  sendApiRequest();
+};
+
+function catHist () {
+  choiceCat = 20; 
+  sendApiRequest();
+};
+
+function catArt () {
+  choiceCat = 25; 
+  sendApiRequest();
+};
+
+function catSci () {
+  choiceCat = 17; 
+  sendApiRequest();
+};
+
+function catSports () {
+  choiceCat = 21; 
+  sendApiRequest();
+};
+
+
+// Send API Request, log data in console, call useApiData - https://www.youtube.com/watch?v=SgJ_femmsfg
 async function sendApiRequest() {
-    let question = await fetch(`https://opentdb.com/api.php?amount=1&category=9&difficulty=easy&type=multiple`);
+    let question = await fetch(`https://opentdb.com/api.php?amount=1&category=${choiceCat}&difficulty=${difficulty}&type=multiple`);
     // console.log(question);
     let data = await question.json();
     console.log(data);
     useApiData(data);
+    return;
 };
 
 // Use API Data - combines correct & incorrect answers, shuffles them, then displays question and answer buttons/texts
 let wholeCard = document.querySelector("section.card");
 let original = wholeCard.innerHTML;
 let pie = 0;
+let questionNum = 0;
 function useApiData(data) {
     let answrArr = data.results[0].incorrect_answers;
     let correctAns = data.results[0].correct_answer;
@@ -63,7 +108,7 @@ const shuffleArray = answrArr => {
 // Make buttons & answers -- https://www.3schools.in/2022/08/create-multiple-buttons-js.html
 const buttonLetters = ['A', 'B', 'C', 'D'];
 const answers = (answrArr, correctAns) => {
-  for(let i = 0; i< answrArr.length; i++){
+  for(let i = 0; i< answrArr.length; i++) {
     let newBtn = document.createElement('button');
     btnChoice = buttonLetters[i]
     newBtn.innerText = btnChoice;
@@ -81,55 +126,37 @@ const answers = (answrArr, correctAns) => {
  
   // Select Correct Answer
   let chooseAnswer = document.getElementsByClassName("answer");
-  // console.log(chooseAnswer);
   Array.from(chooseAnswer).forEach((answer) => {
     answer.addEventListener('click', evt => {
-      // console.log(evt);
       let answerChoice = answer.innerText;
       let answerBtn = answer.id;
-      // console.log(answerChoice);
       if (answerChoice == correctAns || answerBtn == correctAns) {
         alert("Correct! UR DA BEST!");
         wholeCard.innerHTML = original;
+        questionNum++;
         pie++;
-        console.log(pie);
-        // return;
-        // correct function - pie
+        console.log("Question Num: ", questionNum)
+        console.log("Correct: ", pie);
+        checkWin();
       }
       else {console.log('incorrect');
       alert("INCORRECT");
       wholeCard.innerHTML = original;
       console.log(pie);
-      // return;
+      questionNum++;
+      console.log("Question Num: ", questionNum)
+      console.log("Correct: ", pie);
+      checkWin();
     };
-    // function next q
-    
   })});
 };
 
-
-
-let chooseCat = document.querySelectorAll(".category");
-// Click Event
-// const questions = function() {
-  console.log(chooseCat)
-  chooseCat.forEach((category) => {
-    category.addEventListener('click', evt => {
-      // console.log(evt);
-      let catId = category.getAttribute('id');
-      console.log(catId);
-      sendApiRequest();
-    })});
-  // };
-// questions();
-  // window.onclick = function() {
-  //   let chooseCat = document.getElementsByClassName("category");
-  //   // console.log(chooseCat)
-  //   Array.from(chooseCat).forEach((category) => {
-  //     category.addEventListener('click', evt => {
-  //       // console.log(evt);
-  //       let catId = category.getAttribute('id');
-  //       // console.log(catId);
-  //       sendApiRequest();
-  //     })});
-  //   };
+function checkWin () {
+  if (pie === 6) {
+    alert("WINNER!!!")}
+  else if (questionNum === 10) {
+    alert("You Lose");
+    pie = 0;
+    questionNum = 0;
+  }
+}
